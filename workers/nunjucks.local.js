de2af2ca977c479c943d5cd494aeae4ef3d58aa7
Yury Rybak <yuryrybak97@gmail.com>
2020-04-16 23:03:53 +0300

    Added possibility to have dynamic rendering.

--- workers/nunjucks.local.js
+++ workers/nunjucks.local.js
@@ -1,22 +1,26 @@
 'use strict';
 
 module.exports = (worker, done) => {
-  const nunjucks = require('./nunjucks')(worker);
-  const script = new Function('clone', 'environment', 'input', 'context', 'output', `${nunjucks.script};return output`);
-  let result = null;
-  try {
-    result = script(
-      nunjucks.context.clone,
-      nunjucks.context.environment,
-      nunjucks.context.input,
-      nunjucks.context.context,
-      nunjucks.context.output,
-    );
-  }
-  catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    return done({error: err, resolve: null});
-  }
-  return done({resolve: result});
+  require('./nunjucks')(worker)
+    .then((nunjucks) => {
+      const script = new Function('clone', 'environment', 'input', 'context', 'output', `${nunjucks.script};return output`);
+      let result = null;
+
+      try {
+        result = script(
+          nunjucks.context.clone,
+          nunjucks.context.environment,
+          nunjucks.context.input,
+          nunjucks.context.context,
+          nunjucks.context.output,
+        );
+      }
+      catch (err) {
+        console.log(err.message);
+        console.log(err.stack);
+        return done({error: err, resolve: null});
+      }
+
+      return done({resolve: result});
+    });
 };
